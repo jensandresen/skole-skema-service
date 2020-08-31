aa5c76e8c4546334a2d4d16cd982bc2a03bd1d79
Jens Andresen <jensandresen@gmail.com>
2020-08-31 14:52:57 +0200

    change run script to take argument passing

--- run.sh
+++ run.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
-APP_NAME="skole-skema-service" 
+
+APP_NAME="skole-skema-service"
+HOST_DATA_DIR=${1-"/data"}
+HOST_DATA_FILENAME=${2-"skole-skema-data.json"}
+
 docker build -t $APP_NAME .
+
 docker run -d \
     -p 3001:3000 \
-    -v /data:/data \
-    -e DATAFILE="/data/skole-skema-service.json" \
+    -v ${HOST_DATA_DIR}:/data \
+    -e DATAFILE="/data/${HOST_DATA_FILENAME}" \
     --restart unless-stopped \
     $APP_NAME
